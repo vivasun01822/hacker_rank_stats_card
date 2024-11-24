987133508ea02fe7c6a211066260bbdf412c4cfd
MD. Shariar Hossain Sun <shariar.hossain@vivasoftltd.com>
2024-11-24 13:32:03 +0600

    udpate

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { createGithubCard } = require("./createdGithubCard");
 const { fetchHackerrankData } = require("./fetchData");
 
 (async () => {
-  const username = "samba9274"; // Replace with actual HackerRank username
+  const username = "ShariarHossain"; // Replace with actual HackerRank username
   const logoPath = "assets/hackerrank.jpg";
   const outputFile = `output/${username}_hackerrank_card.png`;
 
