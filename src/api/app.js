4061b01fb9870b071960ed7ae846461f820812f1
MD. Shariar Hossain Sun <shariar.hossain@vivasoftltd.com>
2024-12-28 07:58:04 +0600

    move app.js into api

--- src/api/app.js
+++ src/api/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const { fetchHackerrankData } = require("./fetchData"); // Your existing function
+const { fetchHackerrankData } = require("../fetchData"); // Your existing function
 
 const app = express();
 const upload = multer(); // For processing form data
